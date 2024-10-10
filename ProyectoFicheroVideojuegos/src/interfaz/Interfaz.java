aa9b40d9e4287719085550841860e24034dd8eae
Christian Bizarro <chrbizarro@gmail.com>
2024-10-10 20:32:19 +0200

    Documentación

--- ProyectoFicheroVideojuegos/src/interfaz/Interfaz.java
+++ ProyectoFicheroVideojuegos/src/interfaz/Interfaz.java
@@ -3,6 +3,7 @@ package interfaz;
 import java.util.Scanner;
 
 public class Interfaz {
+	
 	private static Scanner scanner = new Scanner(System.in);
 	 private static VideojuegoController controller = new VideojuegoController();
 	
@@ -18,7 +19,7 @@ public class Interfaz {
 				agregarVideojuego();
 				break;
 			case 2:
-				listarVideojuegos();
+				ListarVideojuegos();
 				break;
 				
 			case 3:
@@ -34,14 +35,24 @@ public class Interfaz {
 			}
 		}while(opcion !=0);
 	}
+	
+	/**
+	 * Muestra el menú de inicio en el que tendrás que seleccionar una opción para
+	 * acceder a una de las secciones
+	 */
 		private static void mostrarMenu() {
 			System.out.println("Elige una opción: ");
 			System.out.println("1. Agregar Videojuego");
 			System.out.println("2. Listar Videojuegos");
 			System.out.println("3. Borrar Videojuego");
 			System.out.println("0. Salir de la aplicación");
+			
 		}
-		
+		/**
+		 * Método agregarVideojuego, su función es ser un menú, pides al usuario
+		 * el nombre del videojuego, la comapñía del videojuego y la nota del mismo
+		 * y el usuario deberá con introducir dicha información, la cual se guardará.
+		 */
 		private static void agregarVideojuego() {
 			System.out.println("Ingrese el nombre del videojuego a crear: ");
 			String nombre = scanner.nextLine();
@@ -53,10 +64,16 @@ public class Interfaz {
 			controller.agregarVideojuego(nombre, compania, nota);
 		}
 		
-		private static void listarVideojuegos() {
-	        controller.listarVideojuegos();
+		/**
+		 * Método para listar los videojuegos creados por el usuario
+		 */
+		private static void ListarVideojuegos() {
+	        controller.ListarVideojuegos();
 	    }
-		
+		/**
+		 * Método para ingresar el nombre del Videojuego y poder borrarlo,
+		 * lo busca por el nombre del videojuego y lo borra
+		 */
 		private static void BorrarJuegos() {
 	        System.out.print("Ingrese el nombre del videojuego a borrar: ");
 	        String nombre = scanner.nextLine();
